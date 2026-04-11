e972135b7783a5e5e1962db0c31b5c1c9403a15c
johannady2 <johannady2@gmail.com>
2026-04-11 20:07:48 +0900

    finished the challenge

--- index.js
+++ index.js
@@ -1,14 +1,56 @@
 import express from "express";
 import bodyParser from "body-parser";
+import pg from "pg";
+
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "world",
+  password: "ChibaKing82",
+  port: 5432,
+});
+
+db.connect();
 
 const app = express();
 const port = 3000;
 
+let visitedCountriesDB = [];
+let totalCountriesVisited = 0;
+let visitedCountriesCountryCodes = [];
+
+db.query("SELECT country_code from visited_countries", (err, res) =>
+{
+  if (err) {
+    console.error("Error executing query", err.stack);
+  } else {
+    visitedCountriesDB = res.rows;
+    totalCountriesVisited = visitedCountriesDB.length;
+
+    //console.log(visitedCountries);
+  }
+  db.end();
+});
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.get("/", async (req, res) => {
+
+app.get("/", async (req, res) =>
+{
   //Write your code here.
+
+/*for (let i = 0; i < visitedCountriesDB.length; i++)
+{
+  visitedCountriesCountryCodes.push(visitedCountriesDB[i].country_code);
+}*/
+
+visitedCountriesDB.forEach(country =>
+{
+  visitedCountriesCountryCodes.push(country.country_code);
+});
+
+  res.render("index.ejs", { countries: visitedCountriesCountryCodes , total: totalCountriesVisited});
 });
 
 app.listen(port, () => {
